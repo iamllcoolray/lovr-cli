5d9e6c789dcd06a857deb7b9c4e9c4baafe18b0a
iamllcoolray <llcoolray24@gmail.com>
2025-02-06 09:51:02 -0500

    added a description and updated the help descriptions for the commands

--- src/main.py
+++ src/main.py
@@ -3,15 +3,17 @@ from commands import new, run, build, get
 
 __version__ = "0.1"
 
+__description__ = "Lovr is a command line interface that manages LÖVE2D projects"
+
 # Create the parser
-parser = argparse.ArgumentParser(description="A simple argparse example")
+parser = argparse.ArgumentParser(description=__description__)
 
 # Add arguments
-parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {__version__}', help="show the version of lovr")
+parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {__version__}', help="show the current version of lovr")
 parser.add_argument('-n', '--new', type=str, help="initializes a new LÖVE2D project")
 parser.add_argument('-r', '--run', action='store_true', help="runs the LÖVE2D project")
-parser.add_argument('-b', '--build', action='store_true', help="builds the LÖVE2D project")
-parser.add_argument('-g', '--get', type=str, help="gets and adds third-party libraries in lib")
+parser.add_argument('-b', '--build', action='store_true', help="builds the LÖVE2D project into the builds directory")
+parser.add_argument('-g', '--get', type=str, help="gets and stores third-party libraries in the lib directory")
 
 # Parse the arguments
 args = parser.parse_args()
