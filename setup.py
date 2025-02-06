f95ce3a8dfef6468367f6539eed410e85916951b
iamllcoolray <llcoolray24@gmail.com>
2025-02-06 09:57:11 -0500

    added a description and more information on author, url and licensing details

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages # type: ignore
 
 setup(
     name="lovr",
     version="0.1",
+    description="Lovr is a command line interface that manages LÖVE2D projects",
+    author="Nobunaga",
+    author_email="iamllcoolray@gmail.com",
+    url="https://github.com/iamllcoolray/lovr-cli",
+    license="MIT",
     packages=find_packages(),
     install_requires=[
         "argparse",
